b3d455056d67a3cb7347f190d3024ac4ea35dffa
yunyun <1159428885@qq.com>
2026-09-19 17:06:47 +0800

    更改配色

--- src/lib/themes/accent/red.js
+++ src/lib/themes/accent/red.js
@@ -14,7 +14,7 @@ const guiColors = {
     'extensions-light': 'hsla(160, 35%, 75%, 1)',
 
     'drop-highlight': '#7c3aed',
-    'menu-bar-background': '#4c1d95',
+    'menu-bar-background': '#2b6fec',
     'menu-bar-background-image': 'none',
     
     /* 平衡红 - 用于警告、错误、停止等 */
